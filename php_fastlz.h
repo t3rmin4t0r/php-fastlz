6dba04b5330bda32e5c1319e8cdc2825baf9096e
Pierre Joye <pierre.php@gmail.com>
2011-01-18 14:41:47 +0100

    - move the libfastlz license file for clarity/visibility

--- php_fastlz.h
+++ php_fastlz.h
@@ -1,4 +1,20 @@
-
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2011 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Gopal Vijayaraghavan <gopal@php.net>                        |
+   +----------------------------------------------------------------------+
+ */
 /* $Id: php_fastlz.h 305998 2010-12-06 09:41:04Z gopalv $ */
 
 #ifndef PHP_FASTLZ_H
