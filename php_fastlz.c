b9d8fec0a41422b4b5fb6306bc15bf7458bf0c0c
Pierre Joye <pierre.php@gmail.com>
2011-01-18 12:11:05 +0100

    - fix windows build

--- php_fastlz.c
+++ php_fastlz.c
@@ -11,8 +11,11 @@
 #include "zend_hash.h"
 #include "ext/standard/info.h"
 #include "SAPI.h"
-#include "stdint.h"
-
+#ifdef PHP_WIN32
+# include "win32/php_stdint.h"
+#else
+# include "stdint.h"
+#endif
 #include "php_fastlz.h"
 #include "libfastlz/fastlz.h"
 
